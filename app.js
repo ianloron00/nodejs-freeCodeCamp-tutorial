2eff1b170aac7fd90f5f53664321410880cabbd7
Ian Loron de Almeida <i198933@dac.unicamp.br>
2023-07-26 22:04:59 -0300

    rout params

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("Hello, world")
+const express = require('express')
+const app = express()
+const {products} = require('./data')
+
+app.get('/', (req,res) => {
+    res.json(products)
+})
+
+app.get('/any', (req,res) => {
+    const newProducts = products.map(
+        (product) => {
+            const {id, name, price} = product
+            return {id, name, price}
+        }
+    )
+    res.json(newProducts)
+})
+
+// use route param to set product ID
+app.get('/any/:productID', (req,res) => {
+    const {productID} = req.params
+    const singleProduct  = products.find(
+        (product) =>product.id === Number(productID)
+    )
+    if (!singleProduct) {
+        return res.status(404).send('Product does not exist')
+    }
+    res.json(singleProduct)
+})
+
+app.listen(5000, () => {
+    console.log("Server is listening on port 5000...")
+})
