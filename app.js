b41f46aca53eef7e8a0cb2717a759ff0a7f9083f
Ian Loron de Almeida <i198933@dac.unicamp.br>
2023-07-27 17:11:07 -0300

    learning POST

--- app.js
+++ app.js
@@ -1,58 +1,40 @@
 const express = require('express')
 const app = express()
-const {products} = require('./data')
-const logger = require('./logger')
+let { people } = require('./data')
 
-const productsPath = '/app/products'
+// static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+//parse json
+app.use(express.json())
 
-// req => middleware => res
-app.use('/app', logger)
-
-app.get('/', (req,res) => {
-    // res.json(products)
-    res.send(`<h1>Home Page</h1><a href="${productsPath}">products</a>`)
+app.get('/api/people', (req, res) => {
+    res.status(200).json({ success: true, data: people })
 })
 
-app.get(`${productsPath}`, (req,res) => {
-    const newProducts = products.map(
-        (product) => {
-            const {id, name, price} = product
-            return {id, name, price}
-        }
-    )
-    res.json(newProducts)
+app.post('/api/people', (req, res) => {
+    const {name} = req.body
+    if (!name)
+    return res.status(400).json( {success: false, msg: 'please provide name value' })
+    
+    res.status(201).json({ success: true, person: name })
 })
 
-// use route param to set product ID
-app.get(`${productsPath}/:productID`, (req,res) => {
-    const {productID} = req.params
-    const singleProduct  = products.find(
-        (product) =>product.id === Number(productID)
-    )
-    if (!singleProduct) {
-        return res.status(404).send('Product does not exist')
+app.post('/api/postman/people', (req, res) => {
+    const { name } = req.body
+    if (!name)  
+        return res.status(400).json({ success: false, msg: 'please, provide name value' })
+    res.status(201).send({ success: true, data: [...people, name] })
+})
+
+app.post('/login', (req, res) => {
+    const { name } = req.body
+    if (name) {
+        return res.status(200).send(`Welcome ${name}`)
     }
-    res.json(singleProduct)
 })
 
-app.get('/app/v1/query', (req,res) => {
-    const { search, limit } = req.query
-    let sortedProducts = [...products]
-
-    if (search) {
-        sortedProducts = sortedProducts.filter(
-            (product) => {
-                return product.name.startsWith(search)
-            }
-        )
-    }
-    if (limit) {
-        sortedProducts = sortedProducts.slice(0, Number(limit))
-    }
-    return sortedProducts.length > 1 
-    ? res.status(200).json(sortedProducts)
-    : res.status(200).json({ success: true, data: [] })
-})
 
 app.listen(5000, () => {
     console.log("Server is listening on port 5000...")
