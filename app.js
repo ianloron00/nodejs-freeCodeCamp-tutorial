4c32b6d1c1f34493e1bac35a000d38cfb02e49c3
Ian Loron de Almeida <i198933@dac.unicamp.br>
2023-07-27 10:18:11 -0300

    understanding middleware

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
 const {products} = require('./data')
+const logger = require('./logger')
+
+const productsPath = '/app/products'
+
+// req => middleware => res
+app.use('/app', logger)
 
 app.get('/', (req,res) => {
-    res.json(products)
+    // res.json(products)
+    res.send(`<h1>Home Page</h1><a href="${productsPath}">products</a>`)
 })
 
-app.get('/any', (req,res) => {
+app.get(`${productsPath}`, (req,res) => {
     const newProducts = products.map(
         (product) => {
             const {id, name, price} = product
@@ -17,7 +24,7 @@ app.get('/any', (req,res) => {
 })
 
 // use route param to set product ID
-app.get('/any/:productID', (req,res) => {
+app.get(`${productsPath}/:productID`, (req,res) => {
     const {productID} = req.params
     const singleProduct  = products.find(
         (product) =>product.id === Number(productID)
@@ -26,6 +33,25 @@ app.get('/any/:productID', (req,res) => {
         return res.status(404).send('Product does not exist')
     }
     res.json(singleProduct)
+})
+
+app.get('/app/v1/query', (req,res) => {
+    const { search, limit } = req.query
+    let sortedProducts = [...products]
+
+    if (search) {
+        sortedProducts = sortedProducts.filter(
+            (product) => {
+                return product.name.startsWith(search)
+            }
+        )
+    }
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    return sortedProducts.length > 1 
+    ? res.status(200).json(sortedProducts)
+    : res.status(200).json({ success: true, data: [] })
 })
 
 app.listen(5000, () => {
