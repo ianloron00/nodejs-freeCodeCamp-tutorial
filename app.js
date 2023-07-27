fb66e38111feea512688f34fa0c543a626d12180
Ian Loron de Almeida <i198933@dac.unicamp.br>
2023-07-27 17:25:43 -0300

    added PUT and DELETE

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ app.use(express.urlencoded({ extended: false }))
 //parse json
 app.use(express.json())
 
-app.get('/api/people', (req, res) => {
+app.get('/api/people/', (req, res) => {
     res.status(200).json({ success: true, data: people })
 })
 
-app.post('/api/people', (req, res) => {
+app.post('/api/people/', (req, res) => {
     const {name} = req.body
     if (!name)
     return res.status(400).json( {success: false, msg: 'please provide name value' })
@@ -35,7 +35,36 @@ app.post('/login', (req, res) => {
     }
 })
 
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
 
+    const person  = people.find((person) => person.id === Number(id))
+
+    if (!person) {
+        return res.status(404).json({ success: false, msg: `no person with id ${id}` })
+    }
+
+    const newPeople = people.map((person) => {
+        if (person.id === Number(id))
+            person.name = name
+        return person
+    })
+
+    res.status(200).json({ success: true, data: newPeople })
+})
+
+app.delete('/api/people/:id', (req, res) => {
+    const person  = people.find((person) => person.id === Number(req.params.id))
+
+    if (!person) {
+        return res.status(404).json({ success: false, msg: `no person with id ${req.params.id}` })
+    }
+
+    const newPeople = people.filter(
+        (person) => person.id !== Number(req.params.id))
+    return res.status(200).json({ sucess: true, data: newPeople })
+})
 app.listen(5000, () => {
     console.log("Server is listening on port 5000...")
 })
